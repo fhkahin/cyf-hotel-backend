7793f144d35caae13591c1a0979ece02b5a91755
Fathi Kahin <fhkahin@gmail.com>
2024-06-21 21:46:13 +0100

    updated

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,8 +4,11 @@
 cd /home/ec2-user/cyf-hotel-backend
 
 # Install Node.js and npm
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+curl -sL https://rpm.nodesource.com/setup_20.x | sudo bash -
 sudo yum install -y nodejs
+
+# Verify the Node.js version
+node -v
 
 # Install project dependencies
 npm install
